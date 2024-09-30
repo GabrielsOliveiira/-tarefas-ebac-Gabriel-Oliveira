13d9f16fd1d8cf6d3e726a4c3baf220eeaf4d8be
Gabriel <gcartola37@gmail.com>
2024-09-30 17:36:18 -0300

    tarefa wrapper

---
+++ src/tarefaWrapper.java
@@ -0,0 +1,10 @@
+public class tarefaWrapper {
+    public static void main(String[] args){
+
+        int tarefa = 33;
+        System.out.println(tarefa);
+
+        Integer boxedTarefa = tarefa;
+        System.out.println(boxedTarefa);
+    }
+}
